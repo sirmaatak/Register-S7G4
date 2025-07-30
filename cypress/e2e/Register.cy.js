ce0d23573ba3e1b0b1c7252847a62f3c61111d69
sirmaatak <sirma.atak@outlook.com>
2025-07-30 19:24:18 +0300

    4.commit includes cypress tests

--- cypress/e2e/Register.cy.js
+++ cypress/e2e/Register.cy.js
@@ -6,8 +6,9 @@ const errorMessages = {
     "8 characters long 1 uppercase & 1 lowercase character and 1 number needed",
 };
 
-describe('Register Tests', () => {
-  it('name input test fail for 2 characters', () => {
+describe('Tests', () => {
+  describe('Error Messages',()=>{
+it('name input test fail for 2 characters', () => {
     cy.visit("http://localhost:5177/");
     cy.get('[data-cy="name-input"]').type('sr');
     cy.contains(errorMessages.name);
@@ -27,4 +28,21 @@ describe('Register Tests', () => {
     cy.get('[data-cy="password-input"]').type('12345');
     cy.contains(errorMessages.password);
   });
+  it('submit button disabled test', () => {
+    cy.visit("http://localhost:5177/");
+    cy.get('[data-cy="name-input"]').type('sr');
+    cy.get('[data-cy="submit-input"]').should("be.disabled");
+    //cy.contains(errorMessages.password);
+  });
+  })
+  describe('Validation',()=>{
+it('is button enable when all inputs are correct', () => {
+    cy.visit("http://localhost:5177/");
+    cy.get('[data-cy="name-input"]').type('deneme');
+    cy.get('[data-cy="surname-input"]').type('deneme');
+    cy.get('[data-cy="email-input"]').type('deneme@gmail.com');
+    cy.get('[data-cy="password-input"]').type('*Deneme123*');
+    cy.get('[data-cy="submit-input"]').should("be.enabled");
+  });
+  })
 })
